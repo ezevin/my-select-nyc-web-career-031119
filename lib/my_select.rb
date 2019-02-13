891836cb2f5b94372c8c633493ba36f8065d836c
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-13 20:28:01 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  # your code here!
  array = []
  if block_given?
- collection.select do |num| num.even?
+ array = collection.select do |num| num.even?
  yield(num)
  end 
  end 
